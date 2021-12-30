581fa1bd144ffc9172049b5c6d2ae46964b0d799
James McBean <james.mcbean@rogers.com>
2021-12-30 10:38:10 -0500

    Initial entry

---
+++ vendor/github.com/jmcbean/celeritas/migrations.go
@@ -0,0 +1,73 @@
+package celeritas
+
+import (
+	"github.com/golang-migrate/migrate/v4"
+	"log"
+	"path/filepath"
+
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func (c *Celeritas) MigrateUp(dsn string) error {
+	rootPath := filepath.ToSlash(c.RootPath)
+	m, err := migrate.New("file://"+rootPath+"/migrations", dsn)
+	if err != nil {
+		return err
+	}
+	defer m.Close()
+
+	if err := m.Up(); err != nil {
+		log.Println("Error running migration:", err)
+		return err
+	}
+
+	return nil
+}
+
+func (c *Celeritas) MigrateDownAll(dsn string) error {
+	rootPath := filepath.ToSlash(c.RootPath)
+	m, err := migrate.New("file://"+rootPath+"/migrations", dsn)
+	if err != nil {
+		return err
+	}
+	defer m.Close()
+
+	if err := m.Down(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Celeritas) Steps(n int, dsn string) error {
+	rootPath := filepath.ToSlash(c.RootPath)
+	m, err := migrate.New("file://"+rootPath+"/migrations", dsn)
+	if err != nil {
+		return err
+	}
+	defer m.Close()
+
+	if err := m.Steps(n); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Celeritas) MigrateForce(dsn string) error {
+	rootPath := filepath.ToSlash(c.RootPath)
+	m, err := migrate.New("file://"+rootPath+"/migrations", dsn)
+	if err != nil {
+		return err
+	}
+	defer m.Close()
+
+	if err := m.Force(-1); err != nil {
+		return err
+	}
+
+	return nil
+}
